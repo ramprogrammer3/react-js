bb1cde9c553f106bbadae87c67e0e4033b9adc1e
ram kumar <rammaniyari@gmail.com>
2023-07-16 10:34:02 +0530

    adding useState Hook in productItem component

--- class2/src/components/ProductItem.jsx
+++ class2/src/components/ProductItem.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ProductItemDate from './ProductItemDate'
 
 const ProductItem = ({ items }) => {
+
+    const [title, setTitle] = useState(items.title)
+
+    const clickHandler = () => {
+        setTitle("Popcorn")
+    }
 
     return (
         <div className='product_item'>
             <div className='product_date'>
                 <ProductItemDate date={items.date} />
             </div>
-            <div className='product_name'> {items.title} </div>
+            <div className='product_name'> {title} </div>
+            <button onClick={clickHandler}> Add to cart </button>
         </div>
     )
 }
