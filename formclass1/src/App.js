9ac395512091596c6b4bcc2fbe8041cfe902f90d
ram kumar <rammaniyari@gmail.com>
2023-07-22 08:19:38 +0530

    basic input validation

--- formclass1/src/App.js
+++ formclass1/src/App.js
@@ -1,8 +1,38 @@
 import React from 'react'
 
 const App = () => {
+
+  const firstNameHandler = (event) => {
+
+    console.log(event.target.value)
+
+    console.log(event.target)
+
+  }
+
+  const lastNameHandler = (event) => {
+
+    console.log(event.target.value)
+  }
+
   return (
-    <div>App</div>
+    <div className='app'>
+      <form action="">
+
+        <input type="text" name=""
+          id=""
+          placeholder='first name'
+          onChange={firstNameHandler}
+        />
+
+        <br /><br />
+
+        <input type="text" placeholder='last name' onChange={lastNameHandler} />
+
+
+      </form>
+
+    </div>
   )
 }
 
