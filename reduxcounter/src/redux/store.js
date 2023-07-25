4c9abef246d80d33097b6328b5815553c6d1cb22
ram kumar <rammaniyari@gmail.com>
2023-07-25 16:06:32 +0530

    counter app deploy on firebase

--- reduxcounter/src/redux/store.js
+++ reduxcounter/src/redux/store.js
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import CounterSlice from "./slices/CounterSlice";
 
 export const store = configureStore({
-    reducer : {
-        counter : CounterSlice,
+    reducer: {
+        counter: CounterSlice,
     }
 })
