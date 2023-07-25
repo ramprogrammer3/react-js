b00004357e2e08cf13127d026a4702b3807926ab
ram kumar <rammaniyari@gmail.com>
2023-07-25 14:19:29 +0530

    counter logic complete

--- reduxcounterapp/src/App.js
+++ reduxcounterapp/src/App.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { decrement, increment } from './redux/slices/CounterSlice';
 
 const App = () => {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
   return (
-    <div>App</div>
+    <div>
+      <div>
+        <button onClick={() => dispatch(increment())}> + </button>
+        <div> {count} </div>
+        <button onClick={() => dispatch(decrement())}> - </button>
+      </div>
+    </div>
   )
 }
 
