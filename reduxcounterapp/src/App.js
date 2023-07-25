4473064e86c5dc92a2002e02a8b44fa81eff1de3
ram kumar <rammaniyari@gmail.com>
2023-07-25 14:32:16 +0530

    adding css

--- reduxcounterapp/src/App.js
+++ reduxcounterapp/src/App.js
@@ -5,13 +5,14 @@ import { decrement, increment } from './redux/slices/CounterSlice';
 const App = () => {
   const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
-
   return (
-    <div>
-      <div>
-        <button onClick={() => dispatch(increment())}> + </button>
-        <div> {count} </div>
-        <button onClick={() => dispatch(decrement())}> - </button>
+    <div className='w-screen h-screen flex gap-4 justify-center items-center flex-col bg-gray-700'>
+      <h1 className='text-2xl text-blue-600'> Increment & Decrement  </h1>
+      <div className='flex gap-x-3 bg-white items-center'>
+        <button onClick={() => dispatch(increment())} className='text-2xl px-5  border-r
+         border-r-gray-700'> + </button>
+        <div className='text-2xl px-5 '> {count} </div>
+        <button onClick={() => dispatch(decrement())} className='text-2xl border-l border-gray-700 px-5'> - </button>
       </div>
     </div>
   )
